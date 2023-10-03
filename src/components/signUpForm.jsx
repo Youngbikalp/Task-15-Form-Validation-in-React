9754b003ed9d13efb4f8becabbcafce3e2886077
Narayanadhikari9 <Narayanadhikari9@gmail.com>
2023-10-02 23:39:32 -0500

    changes made in signup

--- src/components/signUpForm.jsx
+++ src/components/signUpForm.jsx
@@ -1,23 +1,43 @@
 import React, { useState } from "react";
 import "../styles/signUpForm.css";
 function SignUpForm() {
-  const [errors, setErrors] = useState("");
+  const [errors, setErrors] = useState({});
+  const [stateName, setStateName] = useState({fullName: ""})
+  const handleInput = (e) => {
+    setStateName({...stateName, [e.target.name] : e.target.value})
+
+  }
+  function formSubmit(e) {
+    e.preventDefault() 
+    setErrors(formValidation()) 
+    
+  } 
+  function formValidation() {
+
+    let error = {}
+    if(stateName.fullName === "") {
+      error.fullName="name field cannot be empty"
+    }
+
+  }
   return (
     <div className="signUp-container">
       <div className="form-group">
         <h2 className="signUp">Sign Up Page</h2>
-        <form>
+        <form onSubmit={formSubmit}>
           <label for="fullName">Full Name</label>
           <input
+          onChange ={handleInput}
             placeholder="Full Name"
             type="text"
             id="fullName"
             name="fullName"
             
           />
-          <div fullname="errors">
-            Your need to enter your full name!
-          </div>
+        
+        {errors.fullName && <p>{errors.fullName}</p>}
+
+          
 
           <br />
           <label for="email">Email</label>
@@ -39,6 +59,7 @@ function SignUpForm() {
             name="email"
 
           />
+          <button>SignUp</button>
         </form>
       </div>
       <br />
